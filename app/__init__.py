c6a74dc26752d3213a7f5981980ab9cb89db47b5
HarryH20 <harrswag0123@gmail.com>
2024-12-06 13:00:59 -0600

    updated queries

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ def create_app():
     app.secret_key = "secret"
 
     # Config
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Ninja123!@localhost/baseball3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Ninja123!@localhost/baseball4'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
